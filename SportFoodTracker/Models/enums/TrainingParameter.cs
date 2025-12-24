c14b69fdb64c97b28a1532468434b362e2bd99c0
Carsten <engelhardtcarsten00@gmail.com>
2025-12-24 17:58:59 +0100

    add trainingparameter to enum

---
+++ SportFoodTracker/Models/enums/TrainingParameter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportFoodTracker.Models.enums
+{
+    public enum TrainingParameter
+    {
+        Repetition,
+        Meter,
+        Time
+    }
+}
