826da665fffeece1f2a600a26429224745c2b6cb
Carsten <engelhardtcarsten00@gmail.com>
2025-12-24 17:59:16 +0100

    add new exercise properties

--- SportFoodTracker/Models/Sportsplan/ExerciseModel.cs
+++ SportFoodTracker/Models/Sportsplan/ExerciseModel.cs
@@ -1,4 +1,5 @@
-﻿using SQLite;
+﻿using SportFoodTracker.Models.enums;
+using SQLite;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,9 +13,20 @@ namespace SportFoodTracker.Models.Sportsplan
         [PrimaryKey]
         [AutoIncrement]
         public int Id { get; set; }
+        public int InternalId { get; set; }
         public string Description { get; set; } = string.Empty;
-        public int Repetition{ get; set; }
+        public int Repetition { get; set; }
+        public int Meter { get; set; }
+        public int TimeInSeconds { get; set; }
         public int Sets { get; set; }
         public int TrainingsplanId { get; set; }
+        public bool IsCreated { get; set; }
+        public TrainingParameter trainingParameter { get; set; }
+        [Ignore]
+        public TimeOnly Time
+        {
+            get => TimeOnly.FromTimeSpan(TimeSpan.FromSeconds(TimeInSeconds));
+            set => TimeInSeconds = (int)value.ToTimeSpan().TotalSeconds;
+        }
     }
 }
